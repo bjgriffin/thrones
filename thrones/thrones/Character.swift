0fb24810443e60f8ad72b9ff1a5a3d0d237fd30e
BJ Griffin <bethurel.griffin.jr@gmail.com>
2016-02-09 22:19:11 -0500

    Initial Project Setup
    *Added Character Objects

---
+++ thrones/thrones/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Character.swift
+//  thrones
+//
+//  Created by BJ Griffin on 2/9/16.
+//  Copyright © 2016 BJ Griffin. All rights reserved.
+//
+
+import UIKit
+
+class Character: NSObject {
+    var name: String?
+    var image: String?
+    var detail: String?
+    
+}
